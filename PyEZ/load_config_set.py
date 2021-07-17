2812aa47354308a4355673f8f2340d8250cfadcf
Bal <olahbalazs@outlook.com>
2021-07-17 18:50:31 +0100

    Create load_config_set.py
    Configure Junos using the 'with' syntax.

---
+++ PyEZ/load_config_set.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from jnpr.junos import Device
+from jnpr.junos.utils.config import Config
+
+if __name__ == '__main__':
+  data = 'set system services netconf traceoptions file test.log'
+  with Device(host='172.25.11.1', user='lab', passwd='lab123') as dev:
+    with Config(dev, mode="exclusive") as conf:
+      conf.load(data, format='set')
+      diff = conf.diff()
+      if diff is None:
+        print("Configuration already up to date.")
+      else:
+        print(diff)
+        conf.commit()
